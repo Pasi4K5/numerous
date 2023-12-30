d202c3839c53aa33ee424f95fbc9b942e8cef0e8
Pasi4K5 <pascal.pomper03@gmail.com>
2023-12-30 02:16:41 +0100

    Log slash commands

---
+++ Numerous/Discord/Events/EventHandler.Logger.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using Numerous.Util;
+using Serilog;
+
+namespace Numerous.Discord.Events;
+
+public partial class DiscordEventHandler
+{
+    [Init]
+    private void Logger_Init()
+    {
+        _client.SlashCommandExecuted += LogSlashCommand;
+    }
+
+    private async Task LogSlashCommand(SocketSlashCommand cmd)
+    {
+        var guild = cmd.GuildId is not null
+            ? await _client.Rest.GetGuildAsync(cmd.GuildId.Value)
+            : null;
+
+        Log.Information(
+            """User "{User}" (ID: {Uid}) executed slash command "{Cmd}" with args {Args} in channel "{Channel}" (ID: {ChannelId}) in guild "{Guild}" (ID: {GuildId})""",
+            cmd.User.Username,
+            cmd.User.Id,
+            cmd.Data.Name,
+            cmd.Data.Options.ToLogString(),
+            cmd.Channel,
+            cmd.ChannelId,
+            guild?.Name ?? "null",
+            cmd.GuildId);
+    }
+}
