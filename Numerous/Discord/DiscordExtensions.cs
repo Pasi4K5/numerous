675ed682ddbf51e35e82a758b1ebc78fe8c77eb7
Pasi4K5 <pascal.pomper03@gmail.com>
2023-12-30 02:35:18 +0100

    Add `ToLogString()` extension method

--- Numerous/Discord/DiscordExtensions.cs
+++ Numerous/Discord/DiscordExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.WebSocket;
 
 namespace Numerous.Discord;
 
@@ -7,5 +8,26 @@ public static class DiscordExtensions
     public static async Task ReplyAsync(this IMessage message, string text)
     {
         await message.Channel.SendMessageAsync(text, messageReference: new(message.Id));
+    }
+
+    public static string ToLogString(this IReadOnlyCollection<SocketSlashCommandDataOption> options)
+    {
+        var s = "";
+
+        foreach (var option in options)
+        {
+            s += string.IsNullOrEmpty(option.Value?.ToString())
+                ? option.Name
+                : $"{option.Name}: \"{option.Value}\", ";
+
+            if (option.Options.Count > 0)
+            {
+                s += ", " + option.Options.ToLogString() + ", ";
+            }
+        }
+
+        s = $"[{s[..^2]}]";
+
+        return s;
     }
 }
