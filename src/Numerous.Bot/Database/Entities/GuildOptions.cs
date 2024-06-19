25ea9caab7a6b8639ebcd08e095e2e39f7616684
Pasi4K5 <47356629+Pasi4K5@users.noreply.github.com>
2024-06-19 16:09:14 +0200

    Ignore extra elements from database for `GuildOptions`

--- src/Numerous.Bot/Database/Entities/GuildOptions.cs
+++ src/Numerous.Bot/Database/Entities/GuildOptions.cs
@@ -3,12 +3,14 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+using MongoDB.Bson.Serialization.Attributes;
 using Numerous.Bot.ApiClients.Osu;
 
 namespace Numerous.Bot.Database.Entities;
 
 // TODO: Remove comment
 // ReSharper disable UnusedMember.Global
+[BsonIgnoreExtraElements]
 public sealed record GuildOptions : DbEntity<ulong>
 {
     public bool TrackMessages { get; init; }
